31621ccd7dd6e196e229aa9dcb0457e4f532837e
Eric Tang <eric.tang2005@gmail.com>
2014-03-13 16:35:53 +1000

    a bug fix in the proc where set a value with multiple return rows

--- sql/update_rev_proc.sql
+++ sql/update_rev_proc.sql
@@ -19,13 +19,13 @@ WHILE i<n DO
   SET pageId = (SELECT page_id from page LIMIT i,1);
   SET revCount = (select count(*) from revision where rev_id = (select page_latest from page where page_id=pageID));
   IF (revCount = 0) THEN
-  	SELECT cast(pageId as char)' contains not valid revision id';
+  	SELECT cast(i as char), cast(pageId as char)' contains not valid revision id';
   	SET revCount = (select count(*) from revision where rev_page=pageId);
   	IF (revCount > 0) THEN
   	  SET pageLatest = (SELECT page_latest from page where page_id=pageId);
   	  IF (pageLatest > 0) THEN
   	    SELECT cast(pageId as char)' updating latest revision id of page';
-        SET revId = (select rev_id from revision where rev_page=pageId and rev_parent_id=pageLatest);
+        SET revId = (select rev_id from revision where rev_page=pageId and rev_parent_id=pageLatest order by rev_id desc limit 1);
         UPDATE page SET page_latest=revId where page_id=pageId;
 	  END IF;
 	END IF;
